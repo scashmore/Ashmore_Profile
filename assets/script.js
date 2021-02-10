fde04b13f512d30861903d8338dfff7673b45a99
Susan Ashmore <scashmorecitcom@gmail.com>
2021-02-09 23:56:46 -0500

    added some content

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 //to change header color
 
-const header = document.getElementById('header');
+const header = document.getElementById('body');
 const colorChanger = setInterval(() => {
 
- const colors = ['none', 'blue','red','green']
+ const colors = ['none', 'blue','red']
   colors.forEach(color => header.classList.remove(color));
-  header.classList.add(colors[Math.floor(Math.random()*4)])
+  header.classList.add(colors[Math.floor(Math.random()*3)])
 
 },3000);
